5c43265ca4e3bb42ff9c2a467c697a8ba7867735
apawlik <aleksandra.n.pawlik@gmail.com>
2014-01-10 17:36:45 +0000

    Fixed the script

--- setup/install_nano_GitBash.sh
+++ setup/install_nano_GitBash.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #A simple script for installing nano for Git Bash. Based on the instructions from https://coderwall.com/p/ee-law
 
 curl -L -O http://www.nano-editor.org/dist/v2.2/NT/nano-2.2.6.zip
@@ -7,4 +8,4 @@ unzip nano-2.2.6.zip -d nanoDIR
 mv nanoDIR ~/bin
 rm nano-2.2.6.zip
 
-echo -e '#!/bin/sh \n exec ~/bin/nanoDIR/nano.exe' > ~/bin/nano
+echo -e '#!/bin/bash \n exec ~/bin/nanoDIR/nano.exe' > ~/bin/nano
